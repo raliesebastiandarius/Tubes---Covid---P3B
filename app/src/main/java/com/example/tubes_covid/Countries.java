5fad4583843700a01c84622f06890db33d0108e1
vinsontandraa <vtandra7@Gmail.com>
2021-01-12 20:49:25 +0700

    buat list

---
+++ app/src/main/java/com/example/tubes_covid/Countries.java
@@ -0,0 +1,48 @@
+package com.example.tubes_covid;
+
+public class Countries {
+    private String nama;
+    private String sembuh;
+    private String meninggal;
+    private String jumlahPos;
+
+    public Countries(String nama, String sembuh, String meninggal, String jumlahPos) {
+        this.nama = nama;
+        this.sembuh = sembuh;
+        this.meninggal = meninggal;
+        this.jumlahPos = jumlahPos;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getSembuh() {
+        return sembuh;
+    }
+
+    public void setSembuh(String sembuh) {
+        this.sembuh = sembuh;
+    }
+
+    public String getMeninggal() {
+        return meninggal;
+    }
+
+    public void setMeninggal(String meninggal) {
+        this.meninggal = meninggal;
+    }
+
+    public String getJumlahPos() {
+        return jumlahPos;
+    }
+
+    public void setJumlahPos(String jumlahPos) {
+        this.jumlahPos = jumlahPos;
+    }
+}
+
--- app/src/main/java/com/example/tubes_covid/CountryFragment.java
+++ app/src/main/java/com/example/tubes_covid/CountryFragment.java
@@ -1,13 +1,19 @@
 package com.example.tubes_covid;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
-public class CountryFragment extends Fragment {
+public class CountryFragment extends Fragment  {
+    protected ListView listMenu;
+    protected ListAdapter adapter;
     protected FragmentListener listener;
     protected MainActivity activity;
 
@@ -16,13 +22,23 @@ public class CountryFragment extends Fragment {
         this.activity = activity;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        View view = inflater.inflate(R.layout.fragment_country,container,false);
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof FragmentListener) {
+            this.listener = (FragmentListener) context;
+        }
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_country, container, false);
+        this.listMenu = view.findViewById(R.id.lv_item);
+        this.adapter = new ListAdapter(getActivity());
+        this.listMenu.setAdapter(this.adapter);
+        this.adapter.addLine("Indonesia", "100","100","0");
+        this.adapter.addLine("Indonesia", "100","100","0");
+        this.adapter.addLine("Indonesia", "100","100","0");
 
         return view;
     }
 
-
 }
-
