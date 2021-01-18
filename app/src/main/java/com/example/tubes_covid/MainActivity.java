78e64c768aa3a34f1166bfadfb34e7b2aa64fc23
ThomasChristianHaeryono <thomaschristian2207@gmail.com>
2021-01-18 20:29:18 +0700

    News

--- app/src/main/java/com/example/tubes_covid/MainActivity.java
+++ app/src/main/java/com/example/tubes_covid/MainActivity.java
@@ -10,12 +10,13 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.telecom.Call;
 import android.widget.Toast;
 
 import java.util.List;
 
-import retrofit2.Call;
-import retrofit2.Callback;
+//import retrofit2.Call;
+////import retrofit2.Callback;
 
 public class MainActivity extends AppCompatActivity implements FragmentListener {
 
@@ -29,9 +30,9 @@ public class MainActivity extends AppCompatActivity implements FragmentListener 
     Toolbar toolbar;
     protected DrawerLayout drawer;
     protected String url;
-    private CustomAdapter customAdapter;
-    private RecyclerView recyclerView;
-    progressDialog progressDialog;
+//    private CustomAdapter customAdapter;
+//    private RecyclerView recyclerView;
+//    progressDialog progressDialog;
 
 
     @Override
@@ -39,9 +40,9 @@ public class MainActivity extends AppCompatActivity implements FragmentListener 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        progressDialog = new progressDialog(MainActivity.this);
-        progressDialog.setMessage("Loading....");
-        progressDialog.show();
+//        progressDialog = new progressDialog(MainActivity.this);
+//        progressDialog.setMessage("Loading....");
+//        progressDialog.show();
         this.fragment1 = new MainFragment();
         this.fragmentCountry = new CountryFragment(this);
         this.fragmentNews = new NewsFragment(this);
@@ -51,27 +52,27 @@ public class MainActivity extends AppCompatActivity implements FragmentListener 
         this.gejalaFragment = new GejalaFragment(this);
 
         GetDataService service = RetrofitClientInstance.getRetrofitInstance().create(GetDataService.class);
-        Call<List<Global>> call = service.getTotalConfirmed();
-        call.enqueue(new Callback<List<Global>>() {
-            @Override
-            public void onResponse(Call<List<Global>> call, Response<List<Global>> response) {
-                progressDialog.dismiss();
-                generateDataList(response.body());
-            }
-
-            @Override
-            public void onFailure(Call<List<Global>> call, Throwable t) {
-                progressDialog.dismiss();
-                Toast.makeText(MainActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        private void generateDataList(List<Global> global) {
-            recyclerView = findViewById(R.id.customRecyclerView);
-            adapter = new CustomAdapter(this,global);
-            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.recyclerView.setLayoutManager(layoutManager);
-            recyclerView.setAdapter(adapter);
-        }
+//        Call<List<Global>> call = service.getTotalConfirmed();
+//        call.enqueue(new Callback<List<Global>>() {
+//            @Override
+//            public void onResponse(Call<List<Global>> call, Response<List<Global>> response) {
+//                progressDialog.dismiss();
+//                generateDataList(response.body());
+//            }
+//
+//            @Override
+//            public void onFailure(Call<List<Global>> call, Throwable t) {
+//                progressDialog.dismiss();
+//                Toast.makeText(MainActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        private void generateDataList(List<Global> global) {
+//            recyclerView = findViewById(R.id.customRecyclerView);
+//            adapter = new CustomAdapter(this,global);
+//            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.recyclerView.setLayoutManager(layoutManager);
+//            recyclerView.setAdapter(adapter);
+//        }
         FragmentTransaction ft = this.fragmentManager.beginTransaction();
         ft.add(R.id.fragment_container, this.fragment1).addToBackStack(null).commit();
 
--- app/src/main/java/com/example/tubes_covid/NewsFragment.java
+++ app/src/main/java/com/example/tubes_covid/NewsFragment.java
@@ -1,27 +1,69 @@
 package com.example.tubes_covid;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
-public class NewsFragment extends Fragment {
+public class NewsFragment extends Fragment{
     protected FragmentListener listener;
     protected MainActivity activity;
+    protected TextView berita1, berita2, berita3, berita4, berita5;
+    protected ImageView gambarBerita1, gambarBerita2, gambarBerita3, gambarBerita4, gambarBerita5;
 
-    public NewsFragment(MainActivity activity)
-    {
+    public NewsFragment(MainActivity activity) {
         this.activity = activity;
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        View view = inflater.inflate(R.layout.fragment_news,container,false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_news, container, false);
+
+        this.berita1 = view.findViewById(R.id.berita1);
+        this.berita2 = view.findViewById(R.id.berita2);
+        this.berita3 = view.findViewById(R.id.berita3);
+        this.berita4 = view.findViewById(R.id.berita4);
+        this.berita5 = view.findViewById(R.id.berita5);
+
+        this.gambarBerita1 = view.findViewById(R.id.gambar1);
+        this.gambarBerita2 = view.findViewById(R.id.gambar2);
+        this.gambarBerita3 = view.findViewById(R.id.gambar3);
+        this.gambarBerita4 = view.findViewById(R.id.gambar4);
+        this.gambarBerita5 = view.findViewById(R.id.gambar5);
+
+        berita1.setText(Html.fromHtml(
+
+                        "<a href=\"https://covid19.go.id/p/berita/menu-makanan-pasien-rs-darurat-wisma-atlet-jadi-perhatian-ketua-gugasnas\">Menu Makanan Pasien RS Darurat Wisma Atlet Jadi Perhatian Ketua Gugasnas</a> "));
+        berita1.setMovementMethod(LinkMovementMethod.getInstance());
+
+        berita2.setText(Html.fromHtml(
+
+                "<a href=\"https://covid19.go.id/p/berita/covid-19-mewabah-doni-monardo-ingatkan-pentingnya-disiplin-kolektif\">Covid-19 Mewabah, Doni Monardo Ingatkan Pentingnya Disiplin Kolektif</a> "));
+        berita2.setMovementMethod(LinkMovementMethod.getInstance());
+
+        berita3.setText(Html.fromHtml(
+
+                "<a href=\"https://covid19.go.id/p/berita/lima-arahan-presiden-terkait-penerapan-adaptasi-kebiasaan-baru\">Lima Arahan Presiden terkait Penerapan Adaptasi Kebiasaan Baru</a> "));
+        berita3.setMovementMethod(LinkMovementMethod.getInstance());
+
+        berita4.setText(Html.fromHtml(
+
+                "<a href=\"https://covid19.go.id/p/berita/dokter-reisa-penggunaan-rapid-test-tidak-digunakan-untuk-kepentingan-diagnostik\">Dokter Reisa: Penggunaan Rapid Test Tidak Digunakan Untuk Kepentingan Diagnostik</a> "));
+        berita4.setMovementMethod(LinkMovementMethod.getInstance());
+
+        berita5.setText(Html.fromHtml(
+
+                "<a href=\"https://covid19.go.id/p/berita/presiden-ingatkan-daerah-untuk-terus-kendalikan-penyebaran-covid-19\">Presiden Ingatkan Daerah untuk Terus Kendalikan Penyebaran Covid-19</a> "));
+        berita5.setMovementMethod(LinkMovementMethod.getInstance());
+
 
         return view;
     }
-
 
 }
